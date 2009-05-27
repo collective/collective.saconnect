b3de96f39097575d11334ea5413f087b5bc6de5b
ldr <ldr@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-27 16:06:28 +0000

    z3c.saconfig support

--- collective.saconnect/trunk/collective/saconnect/interfaces.py
+++ collective.saconnect/trunk/collective/saconnect/interfaces.py
@@ -1,4 +1,8 @@
 from zope.interface.common import mapping
+from z3c.saconfig.interfaces import IEngineFactory, ISiteScopedSession
 
 class ISQLAlchemyConnectionStrings(mapping.IMapping):
     """A collection of SQLAlchemy connection strings"""
+
+class ISiteScopedSessionEngineFactory(ISiteScopedSession, IEngineFactory):
+    """All in one z3c.saconfig utility"""

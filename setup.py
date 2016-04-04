d76a296aa239834ccff501459f8ce0ed1459df22
Peter Holzer <hpeter@agitator.com>
2016-04-04 10:33:25 +0200

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '2.0.1'
+version = '2.0.2.dev0'
 
 long_desc = open("README.rst").read() + "\n"
 long_desc += open(os.path.join("CHANGES.rst")).read()
